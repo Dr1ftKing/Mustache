5ab6d2500380dfb4f9d3ab5dc652dff02243e8d3
Juan <juanlopez325798@gmail.com>
2026-03-29 23:11:15 -0400

    mustache draws to face

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import cv2
 
+def overlay_png(background, overlay, x, y):
+    h, w = overlay.shape[:2]
+    bh, bw = background.shape[:2]
+
+    x1, y1 = max(x, 0), max(y, 0)
+    x2, y2 = min(x + w, bw), min(y + h, bh)
+    ox1, oy1 = x1 - x, y1 -y
+    ox2, oy2 = ox1 + (x2 - x1), oy1 + (y2 -y1)
+
+    if x2 <= x1 or y2 <= y1:
+        return
+    
+    overlay_crop = overlay[oy1:oy2, ox1: ox2]
+    bg_crop = background[y1:y2, x1:x2]
+
+    alpha = overlay_crop[:, :, 3:4] /255.0
+    overlay_rgb = overlay_crop[:, :, :3]
+
+    background[y1:y2, x1:x2] = (overlay_rgb * alpha + bg_crop * (1 - alpha)).astype('uint8')
+
+
 cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(cascade_path)
+
+mustache = cv2.imread("curly-black-mustache-free-png.png", cv2.IMREAD_UNCHANGED)
+
 
 cap = cv2.VideoCapture(0)
 
@@ -14,9 +38,18 @@ while True:
     frame = cv2.flip(frame, 1)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(80,80))
+    
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        mw = int(w * 0.8)
+        mh = int (mw * mustache.shape[0] / mustache.shape[1])
+        resized_mustache = cv2.resize(mustache, (mw, mh))
+
+        mx = x + (w - mw) // 2
+        my = y + int(h * 0.4)
+        
+        overlay_png(frame, resized_mustache, mx, my)
 
     cv2.imshow("Mustache Cam", frame)
 
